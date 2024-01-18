05592a1048f74ceda73c9b73b00ad0f81c62634e
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-01-18 18:12:14 +0200

    add

--- api.js
+++ api.js
@@ -1,3 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNjdOc7gZnGMAT6LQonaE9tcfNCn6eyDg",
+  authDomain: "vanlife-d7e39.firebaseapp.com",
+  projectId: "vanlife-d7e39",
+  storageBucket: "vanlife-d7e39.appspot.com",
+  messagingSenderId: "89604337340",
+  appId: "1:89604337340:web:404b143ec365559b396ab9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
+
+
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
